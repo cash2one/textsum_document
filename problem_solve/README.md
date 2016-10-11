c05114884617d01109000668dafa8296155a18df
zhongtao <zhongtao0329@gmail.com>
2016-10-11 22:15:51 +0800

    update

---
+++ problem_solve/README.md
@@ -0,0 +1,9 @@
+# Problem solve
+
+## initial embedding problem 
+Please check the code in [here](https://github.com/tensorflow/models/blob/master/textsum/seq2seq_attention_model.py) in class Seq2seqAttentionModel and function _add_seq2seq(self)
+    # Embedding shared by the input and outputs.
+    with tf.variable_scope('embedding'), tf.device('/cpu:0'):
+    embedding = tf.get_variable('embedding', [vsize, hps.emb_dim], dtype=tf.float32,initializer=tf.truncated_normal_initializer(stddev=1e-4))
+    emb_encoder_inputs = [tf.nn.embedding_lookup(embedding, x) for x in encoder_inputs]
+    emb_decoder_inputs = [tf.nn.embedding_lookup(embedding, x) for x in decoder_inputs]
---
+++ problem_solve/example.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+import numpy as np
+
+x = tf.placeholder(tf.float32, [2,2])
+with tf.variable_scope('train'):
+    w1 = tf.get_variable('w1', [2,2])
+  #  b = tf.constant(np.array([[1,2],[3,4]], np.float32), name='b') # comment this code has the same function with the next three line codes
+    init = np.array([[1,2],[3,4]])
+    b = tf.get_variable('b', [2,2], initializer=tf.constant_initializer(init))
+    b = tf.stop_gradient(b) # we use tf.stop_gradient(b) to fix b
+if w1.name == 'train/w1:0':
+    print 'good'
+#tf.scalar_summary('w1', 1)
+#tf.scalar_summary('b', b)
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
+loss = tf.reduce_sum(tf.square(x*w1-b*x))
+tf.scalar_summary('loss', loss)
+summary  = tf.merge_all_summaries()
+summary_writer = tf.train.SummaryWriter('./summary')
+
+with tf.Session() as sess:
+    sess.run(tf.initialize_all_variables())
+    for i in range(100):
+        if i%100 == 0:
+            print i
+        feed_dict = {x: np.ones([2,2])}
+        _, summary_str = sess.run([optimizer.minimize(loss), summary], feed_dict=feed_dict)
+        print sess.run(w1)
+        print sess.run(b)
+        summary_writer.add_summary(summary_str)
+    
