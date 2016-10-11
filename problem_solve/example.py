520bd317dd7d713ecdf31a794046d639195abb34
zhongtao <zhongtao0329@gmail.com>
2016-10-11 22:55:18 +0800

    update example.py

--- problem_solve/example.py
+++ problem_solve/example.py
@@ -4,10 +4,22 @@ import numpy as np
 x = tf.placeholder(tf.float32, [2,2])
 with tf.variable_scope('train'):
     w1 = tf.get_variable('w1', [2,2])
+#---------------------1-----------------------
   #  b = tf.constant(np.array([[1,2],[3,4]], np.float32), name='b') # comment this code has the same function with the next three line codes
-    init = np.array([[1,2],[3,4]])
-    b = tf.get_variable('b', [2,2], initializer=tf.constant_initializer(init))
-    b = tf.stop_gradient(b) # we use tf.stop_gradient(b) to fix b
+#---------------------1---------------------
+#---------------------2--------------------
+ #   init = np.array([[1,2],[3,4]])
+ #   b = tf.get_variable('b', [2,2], initializer=tf.constant_initializer(init))
+ #   b = tf.stop_gradient(b) # we use tf.stop_gradient(b) to fix b
+#--------------------2----------------------
+# The block code in 1, 2, 3 has the same function
+#--------------------3----------------------
+    init = np.array([[1,2],[3,4], [5,6]])
+    B = tf.get_variable('B', [3,2], initializer=tf.constant_initializer(init))
+    B = tf.stop_gradient(B) # we use tf.stop_gradient(B) to fix B
+    b = tf.nn.embedding_lookup(B, [1,2])
+#-------------------3------------------------
+
 if w1.name == 'train/w1:0':
     print 'good'
 #tf.scalar_summary('w1', 1)
